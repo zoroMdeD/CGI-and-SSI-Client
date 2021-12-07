b6a253408ef3846c77604589ba141f0bf2746e0b
Морозов Максим <mmorozo@ask.ru>
2021-12-07 12:37:48 +0300

    Initial commit

---
+++ TEST_ETHERNET/TEST_ETHERNET/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TEST_ETHERNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(350, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 107);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 190);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(425, 112);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "GET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_ClickAsync);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(96, 319);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "POST";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(327, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_ClickAsync);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(356, 125);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(23, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_ClickAsync);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(385, 125);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(23, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_ClickAsync);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(414, 125);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(23, 23);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "4";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_ClickAsync);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(519, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(788, 748);
+            this.webBrowser1.TabIndex = 10;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 772);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ TEST_ETHERNET/TEST_ETHERNET/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace TEST_ETHERNET
+{
+    public partial class Form1 : Form
+    {
+        bool push1 = false;
+        bool push2 = false;
+        bool push3 = false;
+        bool push4 = false;
+        //System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            msg("Client Started");
+
+            //clientSocket.Connect("192.168.0.120", 80);
+
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //NetworkStream serverStream = clientSocket.GetStream();
+
+            //byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+
+            //serverStream.Write(outStream, 0, outStream.Length);
+
+            //serverStream.Flush();
+
+
+
+            //byte[] inStream = new byte[10025];  //10025
+
+            //serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+
+            //string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+
+            //msg(returndata);
+
+            //textBox2.Text = "";
+
+            //textBox2.Focus();
+        }
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+
+        private async void button2_ClickAsync(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            try
+            {
+                WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=1&led=2&led=3");
+                using (WebResponse response = await request.GetResponseAsync())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                            response.Close();
+                            //Console.WriteLine(reader.ReadToEnd());
+                        }
+
+                    }
+                }
+
+            }
+            catch(WebException)
+            {;}
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WebRequest request = WebRequest.Create("http://192.168.0.120/ledaction.cgi");
+            request.Method = "POST";
+            string sName = "ledtoggle=1";
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(sName);
+            request.ContentType = "/index.shtml";
+            request.ContentLength = byteArray.Length;
+
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+        }
+
+        private async void button4_ClickAsync(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            if (!push1)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=on1");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                webBrowser1.DocumentText = textBox2.Text;
+                                //webBrowser1.DocumentText = reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {;}
+                push1 = true;
+            }
+            else
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=off1");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {;}
+                push1 = false;
+            }
+
+        }
+
+        private async void button5_ClickAsync(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            if (!push2)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=on2");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push2 = true;
+            }
+            else
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=off2");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push2 = false;
+            }
+        }
+
+        private async void button6_ClickAsync(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            if (!push3)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=on3");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push3 = true;
+            }
+            else
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=off3");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push3 = false;
+            }
+        }
+
+        private async void button7_ClickAsync(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            if (!push4)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=on4");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push4 = true;
+            }
+            else
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create("http://192.168.0.120/leds.cgi?led=off4");
+                    using (WebResponse response = await request.GetResponseAsync())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                textBox2.Text += reader.ReadToEnd() + Environment.NewLine;
+                                response.Close();
+                                //Console.WriteLine(reader.ReadToEnd());
+                            }
+
+                        }
+                    }
+
+                }
+                catch (WebException)
+                {; }
+                push4 = false;
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
